f3159ea3f902a8e4d79fd239f05be6ecb839f244
onlyep <250433177@qq.com>
2017-09-04 19:13:51 +0800

    fix

---
+++ src/App.vue
@@ -0,0 +1,70 @@
+<template>
+  <div>
+    <!-- 路由出口 -->
+    <!-- 路由匹配到的组件将渲染在这里 -->
+    <router-view></router-view>
+    <div class="menu-wrapper">
+      <ul class="menu-list">
+        <router-link :to="item.to" v-for="item in menuList" tag="li" class="menu-item">
+          <span class="icon" :class="classMap[item.type]"></span>
+          <span class="text">{{item.name}}</span>
+        </router-link>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+    created() {
+      this.classMap=['homepage','course','history','topic','person']
+    },
+    data() {
+      return {
+        isSelected: false,
+        menuList: [
+          {
+            name: '首页',
+            to: '/homepage',
+            type: 0
+          },
+          {
+            name: '课程',
+            to: '/course',
+            type: 1
+          },
+          {
+            name: '历史',
+            to: '/history',
+            type: 2
+          },
+          {
+            name: '话题',
+            to: '/topic',
+            type: 3
+          },
+          {
+            name: '个人',
+            to: '/person',
+            type: 4
+          }
+        ]
+      }
+    }
+  }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  .menu-wrapper
+    position: absolute
+    left: 0
+    bottom: 0
+    height: 50px
+    width: 100%
+    background: teal
+    .menu-list
+      display: flex
+      .menu-item
+        flex: 1
+</style>
+
--- src/main.js
+++ src/main.js
@@ -2,17 +2,29 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import VueRouter from 'vue-router'
+import HomePage from './components/homepage/homePage'
 
 import './common/stylus/index.styl'
 
-/* eslint-disable no-new */
-// new Vue({
-//   el: '#app',
-//   render: h=> h(App)
-// })
+Vue.use(VueRouter)
+let router = new VueRouter({
+	mode: 'history',
+	routes: [
+		{
+			path: '/',
+			component: HomePage
+		},
+		{
+			path: '/homepage',
+			component: HomePage
+		}
+	]
+})
 
 new Vue({
 	el: '#app',
+	router,
 	template: '<App/>',
 	components: { App }  
 })
