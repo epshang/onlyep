6c81c18d6d26f03d7f3ecd16a487c56fd63aa86e
onlyep <250433177@qq.com>
2017-09-05 01:20:53 +0800

    menu

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,14 @@
     <router-view></router-view>
     <div class="menu-wrapper">
       <ul class="menu-list">
-        <router-link :to="item.to" v-for="item in menuList" tag="li" class="menu-item">
+        <router-link :to="item.to" v-for="(item,index) in menuList" :key="index" tag="li" class="menu-item" active-class="active" >
           <span class="icon" :class="classMap[item.type]"></span>
           <span class="text">{{item.name}}</span>
         </router-link>
       </ul>
+      <div class="history-wrapper">
+        <span class="icon"></span>
+      </div>
     </div>
   </div>
 </template>
@@ -55,16 +58,76 @@
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+
+  @import "./common/stylus/mixin.styl"
+
   .menu-wrapper
     position: absolute
-    left: 0
-    bottom: 0
-    height: 50px
-    width: 100%
-    background: teal
+    left 0
+    bottom 0
+    height 50px
+    width 100%
+    background #131419
     .menu-list
-      display: flex
+      display flex
+      height 100%
       .menu-item
-        flex: 1
+        flex 1
+        height 100%
+        padding-top 2px
+        text-align center
+        .icon
+          display inline-block
+          width 30px
+          height 30px
+          background-size 30px 30px
+          background-repeat no-repeat
+          &.homepage
+            bg-image('./assets/menu/homepage_0')
+          &.course
+            bg-image('./assets/menu/course_0')
+          &.topic
+            bg-image('./assets/menu/topic_0')
+          &.person
+            bg-image('./assets/menu/person_0')
+        .text
+          display block
+          font-size 11px
+          color #6d6b6c
+        &.active
+          .icon
+            &.homepage
+              bg-image('./assets/menu/homepage_1')
+            &.course
+              bg-image('./assets/menu/course_1')
+            &.topic
+              bg-image('./assets/menu/topic_1')
+            &.person
+              bg-image('./assets/menu/person_1')
+          .text
+            color #fdd947
+    .history-wrapper
+      position absolute
+      top -18px
+      left 0
+      bottom 0
+      right 0
+      z-index 10
+      margin auto
+      width 68px
+      height 68px
+      background #131419
+      border-radius 50%
+      text-align center
+      .icon
+        display inline-block
+        margin-top 5px
+        line-height 68px
+        width 53px
+        height 58px
+        background-size 53px 58px
+        background-repeat no-repeat
+        bg-image('./assets/menu/mascot')
+
 </style>
 
